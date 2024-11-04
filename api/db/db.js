231876990e4cd287e39d67bf44553992f72272d8
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2024-11-03 23:15:31 -0300

    Dockerize API

--- api/db/db.js
+++ api/db/db.js
@@ -1,11 +1,11 @@
 // Revisar por qué crashea con las versiones nuevas!!
 var db = require('mysql2-promise')();
- 
+
 db.configure({
-    "host": "localhost",
-    "user": "usuario",
-    "password": "pass1234",
-    "database": "mi_database"
+    "host": process.env.DB_HOST,
+    "user": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE
 });
 
 
@@ -13,10 +13,10 @@ const mysql = require('mysql2');
 
 // Create the connection to database
 const connectionCb = mysql.createConnection({
-  host: 'localhost',
-  user: 'usuario',
-  password: "pass1234",
-  database: 'mi_database',
+  "host": process.env.DB_HOST,
+  "user": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_DATABASE
 });
 
 module.exports = { 
