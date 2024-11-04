5d5d23a0f1cf94d5aff64313704962a9c579cd00
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2024-11-04 16:37:17 -0300

    Add ssl cert

--- api/db/db.js
+++ api/db/db.js
@@ -1,17 +1,22 @@
 // Revisar por qué crashea con las versiones nuevas!!
 var db = require('mysql2-promise')();
 const mysql = require('mysql2');
+const fs = require('node:fs');
 
 const dbConfig = {
   "host": process.env.DB_HOST,
   "user": process.env.DB_USER,
   "password": process.env.DB_PASSWORD,
   "database": process.env.DB_DATABASE
-}
+};
 
+if (process.env.DB_SSL_CERT) {
+  dbConfig.ssl = {
+    ca: fs.readFileSync(process.env.DB_SSL_CERT)
+  };
+} 
 
 db.configure(dbConfig);
-
 const connectionCb = mysql.createConnection(dbConfig);
 
 module.exports = { 
