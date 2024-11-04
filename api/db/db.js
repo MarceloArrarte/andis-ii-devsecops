d3252acce9c7047170e931a53945f0ccc0fad969
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2024-11-04 01:21:54 -0300

    Fix api watch restart

--- api/db/db.js
+++ api/db/db.js
@@ -1,23 +1,18 @@
 // Revisar por qué crashea con las versiones nuevas!!
 var db = require('mysql2-promise')();
-
-db.configure({
-    "host": process.env.DB_HOST,
-    "user": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE
-});
-
-
 const mysql = require('mysql2');
 
-// Create the connection to database
-const connectionCb = mysql.createConnection({
+const dbConfig = {
   "host": process.env.DB_HOST,
   "user": process.env.DB_USER,
   "password": process.env.DB_PASSWORD,
   "database": process.env.DB_DATABASE
-});
+}
+
+
+db.configure(dbConfig);
+
+const connectionCb = mysql.createConnection(dbConfig);
 
 module.exports = { 
     db,
